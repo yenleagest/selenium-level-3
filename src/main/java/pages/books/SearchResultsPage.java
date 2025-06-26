e92a8e87176a0aba8bcc0c9ccfcee98a9a4a9ad5
catssaymeow <catssaymeow@proton.me>
2025-06-26 11:35:54 +0700

    Fix review comments

--- src/main/java/pages/books/SearchResultsPage.java
+++ src/main/java/pages/books/SearchResultsPage.java
@@ -1,14 +1,12 @@
 package pages.books;
 
 import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Objects;
 
-import static com.codeborne.selenide.Condition.image;
 import static com.codeborne.selenide.Selectors.shadowDeepCss;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -16,43 +14,28 @@ import static com.codeborne.selenide.Selenide.$$;
 public class SearchResultsPage extends HomePage {
 
     private final String bookTitle = ".books li .title-container";
-    private final String bookImage = ".books li img";
+    private final String loadedResult = ".books li .placeholder[fadeout]";
 
     @Step("Do all search results contain {keyword}")
     public boolean doAllResultsContainKeyword(String keyword) {
         List<String> titles = getBookTitleFromSearchResults();
-        return titles.stream()
+        return Objects.requireNonNull(titles).stream()
                 .allMatch(title -> title.toLowerCase().contains(keyword.toLowerCase()));
     }
 
     @Step("Get book title from search results")
     private List<String> getBookTitleFromSearchResults() {
-        ElementsCollection images = $$(shadowDeepCss(bookImage)).shouldHave(CollectionCondition.sizeGreaterThan(0));
-        ElementsCollection titles = $$(shadowDeepCss(bookTitle));
-
-        // keep scrolling and checking for new results
-        // if the number of results does not increase after scrolling, increase retry count
-        // if new results appear, reset retry and re-evaluate until max retries is reached
-        int retry = 0;
-        while (retry < 3) {
-            images.last().scrollIntoView("{block: 'center'}");
-            images.forEach(el -> el.shouldBe(image));
-            if (titles.size() == $$(shadowDeepCss(bookTitle)).size()) {
-                retry++;
-            } else {
-                titles = $$(shadowDeepCss(bookTitle));
-                retry = 0;
-            }
+        try {
+            $$(shadowDeepCss(loadedResult)).shouldBe(CollectionCondition.sizeGreaterThan(0));
+        } catch (AssertionError e) {
+            log.error("No search results displayed for the given keyword: {}", e.getMessage());
+            return List.of(); // return an empty list if no results are found
         }
-
-        List<String> extractedTitles = titles.stream()
-                .map(el -> {
-                    el.shouldNotHave(Condition.exactText(""));
-                    return el.getText().trim();
-                })
+        List<String> titles = $$(shadowDeepCss(bookTitle)).stream()
+                .map(el -> el.getText().trim())
                 .toList();
-        log.info("Results title: {}", extractedTitles);
-        return extractedTitles;
+        log.info("Results title: {}", titles);
+        return titles;
     }
 }
 
